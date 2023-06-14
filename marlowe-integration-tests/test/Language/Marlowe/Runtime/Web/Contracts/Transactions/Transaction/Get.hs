373ad1d9629fbdd5cdb62546ec38c7560ea09725
nhenin <nicolas.henin@iohk.io>
2023-06-14 12:44:13 +0200

    [marlowe-integration-tests] renamed modules

--- marlowe-integration-tests/test/Language/Marlowe/Runtime/Web/Contracts/Transactions/Transaction/Get.hs
+++ marlowe-integration-tests/test/Language/Marlowe/Runtime/Web/Contracts/Transactions/Transaction/Get.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
-module Language.Marlowe.Runtime.Web.Contracts.Transactions.Transaction.GetTransaction where
+module Language.Marlowe.Runtime.Web.Contracts.Transactions.Transaction.Get where
 
 import Control.Monad.IO.Class (MonadIO(liftIO))
 import qualified Control.Monad.Reader as Reader
